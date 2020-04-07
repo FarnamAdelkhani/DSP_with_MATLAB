31a0226c29e37ffc741131b0b4e3fd7bcfd3f233
Farnam Adelkhani <Farnam7@gmail.com>
2020-04-06 17:10:26 -0700

    init commit

---
+++ DSP__FIR-Filters-Part-1-of-2/FIR_Filter.m
@@ -0,0 +1,25 @@
+%% Farnam Adelkhani  2017
+% Checks functionality of FIR window design
+% ... create a number realizable FIR filter
+%
+%% Part 1: Check filter design
+    test_lab5a
+%% Part II: Comparative behavior of window filters
+    test_lab5b
+%% Part III: Phone tones
+    test_lab5c
+%% Part IV: Program .m scripts
+disp('************* rectfilt.m  *************')
+type('rectfilt')
+disp('************* hammingfilt.m  *************')
+type('hammingfilt')
+disp('************* kaiserparams.m  *************')
+type('kaiserparams')
+disp('************* kaiserfilt.m  *************')
+type('kaiserfilt')
+disp('************* magdb.m  *************')
+type('magdb')
+disp('************* separate.m  *************')
+type('separate')
+
+
